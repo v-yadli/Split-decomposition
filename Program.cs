9bab54c4769b90309348434bdd9152296b262700
Yatao Li <yatao.li@live.com>
2013-06-15 05:11:06 +0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph();
+            g.Load("TestGraph.txt");
+            var ST = g.SplitDecomposition();
+        }
+    }
+}
